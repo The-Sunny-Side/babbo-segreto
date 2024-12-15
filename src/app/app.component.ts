6baa665e1da4dc4b2a934ee76f4aa6199c13212b
simoneloop <simonelopez@hotmail.it>
2024-12-16 00:24:12 +0100

    init babbo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import * as regaliData from './_files/regali.json';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'babbo_segreto';
+  regali: { [key: string]: string } = regaliData;
+
+  to_show=""
+  onSubmit(e:any){
+    if(this.regali[e]){
+      this.to_show="devi fare un regalo a: "
+      this.to_show+=this.regali[e]
+    }
+    else{
+      this.to_show="mannaia a dio inserisci la password inviata"
+    }
+
+  }
 }
