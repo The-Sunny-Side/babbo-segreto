9acc246bd2ed91cbec2c2cd1710c45ff0f01176a
simoneloop <simonelopez@hotmail.it>
2024-12-23 22:23:51 +0100

    feat auto buildv25

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,5 @@ import { Routes } from '@angular/router';
 import { AppComponent } from './app.component'; // Assicurati che il percorso sia corretto
 
 export const routes: Routes = [
-  { path: '', component: AppComponent }, // Home page
   { path: '**', redirectTo: '' }         // Fallback per rotte sconosciute
 ];
