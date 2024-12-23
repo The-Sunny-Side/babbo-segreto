aaca351f9514f035215f6605eef2b3e7da32eade
simoneloop <simonelopez@hotmail.it>
2024-12-24 00:00:42 +0100

    v37

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
-import { AppComponent } from './app.component'; // Assicurati che il percorso sia corretto
+import { AppComponent } from './app.component'; 
 
 export const routes: Routes = [
-  { path: '**', redirectTo: '' }         // Fallback per rotte sconosciute
+  { path: '**', redirectTo: '' } 
 ];
